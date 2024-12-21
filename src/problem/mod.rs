bb05eaf66baca3c948bffd718af3a7e9af2c3cdd
musleh <khan.muslehuddin0001@gmail.com>
2024-12-21 19:15:38 +0600

    added rotate array solution

--- src/problem/mod.rs
+++ src/problem/mod.rs
@@ -3,4 +3,5 @@ pub mod merge_sorted_array;
 pub mod remove_duplicates_from_sorted_array;
 pub mod remove_duplicates_from_sorted_array_ii;
 pub mod remove_element;
+pub mod rotate_array;
 pub mod two_sum;
---
+++ src/problem/rotate_array.rs
@@ -0,0 +1,37 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn rotate(nums: &mut Vec<i32>, k: i32) {
+        let effective_k = k % nums.len() as i32;
+        nums.rotate_right(effective_k as usize);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rotate_v1() {
+        let mut nums = vec![1, 2, 3, 4, 5, 6, 7];
+        Solution::rotate(&mut nums, 3);
+
+        assert_eq!(nums, vec![5, 6, 7, 1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn rotate_v2() {
+        let mut nums = vec![-1, -100, 3, 99];
+        Solution::rotate(&mut nums, 2);
+
+        assert_eq!(nums, vec![3, 99, -1, -100]);
+    }
+
+    #[test]
+    fn rotate_v3() {
+        let mut nums = vec![-1];
+        Solution::rotate(&mut nums, 2);
+
+        assert_eq!(nums, vec![-1]);
+    }
+}
