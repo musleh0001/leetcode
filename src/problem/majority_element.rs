ee75df22bbd599be0d17549397a631ff7126f30b
musleh <khan.muslehuddin0001@gmail.com>
2024-12-22 13:26:46 +0600

    added remove majority element hashmap solution

--- src/problem/majority_element.rs
+++ src/problem/majority_element.rs
@@ -19,6 +19,19 @@ impl Solution {
 
         m
     }
+
+    pub fn majority_element_v2(nums: Vec<i32>) -> i32 {
+        let mut count = std::collections::HashMap::new();
+        let (mut result_key, mut max_count) = (0, 0);
+
+        for num in nums {
+            let value = count.entry(num).and_modify(|e| *e += 1).or_insert(1);
+            result_key = if *value > max_count { num } else { result_key };
+            max_count = std::cmp::max(*value, max_count);
+        }
+
+        result_key
+    }
 }
 
 #[cfg(test)]
@@ -27,19 +40,19 @@ mod tests {
 
     #[test]
     fn majority_element_v1() {
-        let result = Solution::majority_element(vec![3, 2, 3]);
+        let result = Solution::majority_element_v2(vec![3, 2, 3]);
         assert_eq!(result, 3);
     }
 
     #[test]
     fn majority_element_v2() {
-        let result = Solution::majority_element(vec![2, 2, 1, 1, 1, 2, 2]);
+        let result = Solution::majority_element_v2(vec![2, 2, 1, 1, 1, 2, 2]);
         assert_eq!(result, 2);
     }
 
     #[test]
     fn majority_element_v3() {
-        let result = Solution::majority_element(vec![2, 2, 1, 1, 5, 1, 2, 2]);
+        let result = Solution::majority_element_v2(vec![2, 2, 1, 1, 5, 1, 2, 2]);
         assert_eq!(result, 2);
     }
 }
