6d6996b43a966a4c161af7d8fcb6596c0138fae2
musleh0001 <khan.muslehuddin0001@gmail.com>
2026-08-05 11:10:39 +0600

    remove file fn

--- problems/arrays/merge_sorted_array.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-
-        return None
--- problems/arrays/two_sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: list[int], target: int):
-        seen: dict[int, int] = {}
-
-        for index, num in enumerate(nums):
-            complements = target - num
-            if complements in seen:
-                return [seen[complements], index]
-            seen[complements] = index
